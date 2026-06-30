9e00fe07147a688060350881356b075e2b5eff68
dogemie <zorocrit@gmail.com>
2026-06-30 16:35:59 +0900

    Remove unused image files and scripts related to sparse reconstruction and video processing
    - Deleted multiple PNG images from nerf_data/images directory.
    - Removed binary files from nerf_data/sparse/0 directory.
    - Eliminated sparse_important.py script which handled sparse reconstruction.
    - Deleted video_important.py script responsible for video to image conversion and processing.
    - Cleaned up temporary files including tempCodeRunnerFile.py and point_cloud.ply.

--- Flank_Hyundong/npy_reader.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-poses = np.load("llff_data.npz", allow_pickle=True)
-poses_loaded = poses["focal_lengths"]
-print(poses_loaded)
--- Flank_Hyundong/read_poses.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-pose_path = "poses_bounds.npy"
-
-poses = np.load(pose_path)
-print(poses.shape)
